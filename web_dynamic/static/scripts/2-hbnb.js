025025b7f83d9e095ed15bd0686a74a730bdea8b
Esoteric918 <85411745+Esoteric918@users.noreply.github.com>
2022-01-17 17:34:46 -0600

    Task 3 (#3)
    * 3 init
    
    * task 3
    
    * add file
    
    * update task 3
    
    * 2-hbnb.js swapped to 127 for get
    
    Co-authored-by: Tres Serio <treserio@hotmail.com>

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,46 @@
+const amList = [];
+setTimeout(() => {
+  $('input:checkbox').click(function () {
+    if ($(this).is(':checked')) {
+      amList.push($(this).data('name'));
+    } else {
+      amList.splice(amList.indexOf($(this).data('name')), 1);
+    }
+    // if get h4 of parent's, parent's, parent's, parent?
+    if (amList.length == 0) {
+      $('.amenities h4').text(String.fromCharCode(160));
+    } else {
+      $('.amenities h4').text(amList.sort().join(', '));
+    }
+  });
+}, 200);
+
+$.get('http://127.0.0.1:5001/api/v1/status/', (data) => {
+  if (data.status === 'OK') {
+    console.log(data);
+    $('div#api_status').addClass('available');
+  } else {
+    $('div#api_status').removeClass('available');
+  }
+});
+
+// const callApi = $.ajax('http://0.0.0.0:5001/api/v1/status/');
+//   if (callApi.status === 'OK') {
+//     console.log(callApi);
+//     $('div#api_status').addClass('available');
+//   } else {
+//     $('div#api_status').removeClass('available');
+//     };
+
+
+
+// $(() => {
+//   $.get('http://127.0.0.1:5001/api/v1/status/', (data) => {
+//     if (data.status === 'OK') {
+//       console.log(data);
+//       $('div#api_status').addClass('available');
+//     } else {
+//       $('div#api_status').removeClass('available');
+//     }
+//   });
+// });
